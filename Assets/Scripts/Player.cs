8ecda07b88bc067e3fd084a247a1ce9a7bce3417
EsercitoHaki <esercitohaki2003@gmail.com>
2024-11-03 20:48:12 +0700

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Attack details")]
+    public Vector2[] attackMovement;
+
+    public bool isBusy {get; private set;}
+    [Header("Move info")]
+    public float moveSpeed;
+    public float jumpForce;
+    public float dashSpeed;
+    public float dashDuration;
+
+    [Header("Collision info")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+
+    #region Components
+    public Animator anim { get; private set;}
+    public Rigidbody2D rb { get; private set;}
+
+    public int facingDir { get; private set; } = 1;
+    private bool facingRight = true;
+
+    #endregion
+
+    #region States
+    public PlayerStateMachine stateMachine { get; private set;}
+
+    public PlayerIdleState idleState { get; private set;}
+    public PlayerMoveState moveState { get; private set;}
+    public PlayerJumpState jumpState { get; private set;}
+    public PlayerAirState airState { get; private set;}
+    public PlayerDashState dashState { get; private set; }
+    public PlayerPrimaryAttackState primaryAttack { get; private set;}
+    #endregion
+
+    private void Awake()
+    {
+        stateMachine = new PlayerStateMachine();
+
+        idleState = new PlayerIdleState(this, stateMachine, "Idle");
+        moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState  = new PlayerAirState(this, stateMachine, "Jump");
+        dashState = new PlayerDashState(this, stateMachine, "Dash");
+        primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
+    }
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        stateMachine.currentState.Update();
+        StartCoroutine("BusyFor", .1f);
+    }
+
+    public IEnumerator BusyFor(float _seconds)
+    {
+        isBusy = true;
+        yield return new WaitForSeconds(_seconds);
+        isBusy = false;
+    }
+
+    public void AnimationTrigger() => stateMachine.currentState.AnimationFinishTrigger();
+
+    #region Velocity
+    public void ZeroVelocity()
+    {
+        rb.velocity = new Vector2(0, 0);
+    }
+
+    public void SetVelocity(float _xvelocity, float _yvelocity)
+    {
+        rb.velocity = new Vector2(_xvelocity, _yvelocity);
+        FlipController(_xvelocity);
+    }
+    #endregion
+
+    #region Collision
+
+    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    #endregion
+
+    #region Flip
+    public void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if (_x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (_x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    #endregion
+}
